cf657894a0a8b26c18af2174822013a0234e5d94
Nabin Oli <107109731+nabin2004@users.noreply.github.com>
2025-04-04 10:21:04 +0545

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -2,7 +2,7 @@
 
 # Run Speech to Text fine-tuning using NeMo
 python3 examples/asr/speech_to_text_finetune.py \
-model.tokenizer.update_tokenizer=True \
+model.tokenizer.update_tokenizer=False \
 trainer.devices=-1 \
 trainer.accelerator="gpu" \
 trainer.max_epochs=100
