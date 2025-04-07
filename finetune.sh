e9096269a8058a69a289645e56fba9971aa4cade
Nabin Oli <107109731+nabin2004@users.noreply.github.com>
2025-04-07 16:23:53 +0545

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -5,7 +5,7 @@ python3 examples/asr/speech_to_text_finetune.py \
 model.tokenizer.update_tokenizer=False \
 trainer.devices=-1 \
 trainer.accelerator="gpu" \
-trainer.max_epochs=100
+trainer.max_epochs=20
 # ++model.train_ds.use_semi_sorted_batching=true
 # ++model.train_ds.randomization_factor=0.1
 # trainer.sync_batchnorm= False \
